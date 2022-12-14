dc0cad91ec8465bd7a16d0460fa880fd56ffecf6
Taha Shahid <tahashahid203@gmail.com>
2022-12-14 15:53:29 +0500

    PF Lab 2 assignments

---
+++ projectHeader.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main() {
+
+cout << "***********************************************************************" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                   Taha's Software House                             *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*               We achieve nothing less than excellence               *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "*                                                                     *" << endl;
+cout << "***********************************************************************" << endl;
+}
